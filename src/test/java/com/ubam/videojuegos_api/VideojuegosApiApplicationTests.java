b34d50f7d0f1ce12f388a572d8e39f4ac1febc63
irvin <marquez190705@gmail.com>
2026-02-25 02:59:28 -0600

    Primer Commit : Inicio de proyecto

---
+++ src/test/java/com/ubam/videojuegos_api/VideojuegosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubam.videojuegos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideojuegosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
